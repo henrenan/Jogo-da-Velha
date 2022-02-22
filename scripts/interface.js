6827caca5d93a9f8f5ff3205e14b10254418bbf1
Renan Marques <66074034+henrenan@users.noreply.github.com>
2022-02-22 05:57:33 -0300

    reset

--- scripts/interface.js
+++ scripts/interface.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-        let squares  = document.querySelectorAll(".squares");
+        let squares  = document.querySelectorAll(".square");
 
         squares.forEach((square) => {
             square.addEventListener('click', handleClick);
@@ -9,26 +9,23 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
 function handleClick(event) {
-    console.log(event.target)
 
     let square =  event.target;
     let position = square.id;
 
-    handleMove(position)
-    updadeSquares();
+    if (handleMove(position)) {
+
+        setTimeout(() => {
+        alert(" O jogo Acabou - O Vencedor foi " + playerTime);
+    }, 10);
+
+    };
+    updadeSquares(position);
 }
 
-function updadeSquares(){
+function updadeSquares(position) {
 
-    let squares  = document.querySelectorAll(".square");
-
-        squares.forEach((square) => {
-            let position = square.id;
-            let symbol = board[position];
-
-           if (symbol != ''){
-               square.innerHTML =`<div class='${symbols}'></div>`
-           }
-        })
-
-}
+    let square  = document.getElementById(position.toString());
+    let symbol = board[position];
+    square.innerHTML =`<div class='${symbol}'></div>`
+    }
